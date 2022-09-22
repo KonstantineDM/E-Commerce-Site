5748a286b25687d65d9a8406d9de1823319af34e
KonstantineDM <iteamallard@gmail.com>
2022-09-22 23:55:11 +0300

    added category view, variables to blade files

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index() {
+        return view('index');
+    }
+
+    public function categories() {
+        return view('categories');
+    }
+
+    public function category($category) {
+        return view('category', compact('category'));
+    }
+
+    public function product(string $product = 'Generic Phone') {
+        return view('product', ['product' => $product]);
+    }
 }
