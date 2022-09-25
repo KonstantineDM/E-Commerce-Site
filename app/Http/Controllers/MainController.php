e448949add11a0845123eb18ec1a609010f4f344
KonstantineDM <iteamallard@gmail.com>
2022-09-25 21:43:36 +0300

    WIP: added Eloquent relations

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
     public function index() {
-        return view('index');
+        $products = Product::get();
+        return view('index', compact('products'));
     }
 
     public function categories() {
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,13 +5,15 @@
 <div class="container">
     <div class="starter-template">
         <h1>
-            {{ $category->name }}
+            {{ $category->name }} {{ $category->products->count() }}
         </h1>
         <p>
             {{ $category->description }}
         </p>
         <div class="row">
-            @include('card')
+            @foreach ($category->products as $product)
+                @include('card', compact('product'))
+            @endforeach
         </div>
     </div>
 </div>
